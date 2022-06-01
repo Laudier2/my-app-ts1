86b9816eba759d9f844e6b25da087d36a1d55f9f
Laudier2 <trabalhandooline2019@hotmail.com>
2022-06-01 19:21:19 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+require('./modules/db')
 
 const port = process.env.PORT || 3000;
 
 app.use("/", (req, res) => {
     res.send("Òla Mundo")
 })
+
+app.use(cors())
 
 app.listen(port, () => {
     console.info("url - http:localhost:" + port)
---
+++ src/modules/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+/**
+ * Aqui estamos configurando a conecção da base de dados 
+ * utilizando uma variavel de ambiente para mais segurança.
+ */
+
+//mongoose.connect("mongodb://localhost:27017/Users")
+mongoose.connect(process.env.URL_DB)
+
+  /**
+   * E abaixo estamos utilizando o .then para acibir umamensage no console caso esteja fucionando corretamente 
+   * e o .catch caso aconteca algum erro para mostra qual erro foi.
+   */
+
+  .then(() => {
+    console.log('A conecção com o mango foi bem sucedida OK!')
+  })
+  .catch((err) => {
+    console.log('Houve um erro ao se conectar com o mongo ' + err)
+  })
